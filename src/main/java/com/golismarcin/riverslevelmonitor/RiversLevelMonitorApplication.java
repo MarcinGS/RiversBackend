1ef3ec9528a5a9c05fc068709fdca2709659924c
Marcin <golismarcin@gmail.com>
2023-01-17 21:40:04 +0100

    initialize commit

---
+++ src/main/java/com/golismarcin/riverslevelmonitor/RiversLevelMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.golismarcin.riverslevelmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiversLevelMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiversLevelMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golismarcin/riverslevelmonitor/RiversLevelMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golismarcin.riverslevelmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiversLevelMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1f3054f794633b5a8f4cbf281c373fb14c66e79f
Marcin <golismarcin@gmail.com>
2023-01-25 21:45:47 +0100

    Paginacja

--- src/main/java/com/golismarcin/riverslevelmonitor/river/controller/RiverController.java
+++ src/main/java/com/golismarcin/riverslevelmonitor/river/controller/RiverController.java
@@ -3,10 +3,10 @@ package com.golismarcin.riverslevelmonitor.river.controller;
 import com.golismarcin.riverslevelmonitor.river.model.River;
 import com.golismarcin.riverslevelmonitor.river.service.RiverService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +15,7 @@ public class RiverController {
     private final RiverService riverService;
 
     @GetMapping("/rivers")
-    public List<River> getRivers(){
-        return riverService.getRivers();
+    public Page<River> getRivers(Pageable pageable){
+        return riverService.getRivers(pageable);
     }
 }
--- src/main/java/com/golismarcin/riverslevelmonitor/river/service/RiverService.java
+++ src/main/java/com/golismarcin/riverslevelmonitor/river/service/RiverService.java
@@ -3,9 +3,9 @@ package com.golismarcin.riverslevelmonitor.river.service;
 import com.golismarcin.riverslevelmonitor.river.model.River;
 import com.golismarcin.riverslevelmonitor.river.repository.RiverRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +13,7 @@ public class RiverService {
 
     private final RiverRepository riverRepository;
 
-    public List<River> getRivers(){
-        return riverRepository.findAll();
+    public Page<River> getRivers(Pageable pageable){
+        return riverRepository.findAll(pageable);
     }
 }
